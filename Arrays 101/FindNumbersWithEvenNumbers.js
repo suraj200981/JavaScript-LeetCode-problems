b20d46d29baeea3520d773d573c4a1392b04d730
suraj200981 <suraj200981@yahoo.co.uk>
2022-01-31 18:30:17 +0000

    find numbers with even numbers nearly done

---
+++ Arrays 101/FindNumbersWithEvenNumbers.js
@@ -0,0 +1,23 @@
+//Given an array nums of integers, return how many of them contain an even number of digits.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findNumbers = function(nums) {
+
+
+    var totalEven= 0;
+    for(var i = 0; i < nums.length; i++){
+        if(nums[i] %2===0){
+            totalEven++;
+        
+        }
+    }
+
+    return totalEven;
+    
+};
+
+
+console.log(findNumbers([12,345,2,6,7896]));
