4616a94f39d74e9584311b3edae180fc07a81872
suraj <suraj200981@yahoo.co.uk>
2022-01-31 18:41:56 +0000

    find numbers with even number of digits challenge completed

--- Arrays 101/FindNumbersWithEvenNumbers.js
+++ Arrays 101/FindNumbersWithEvenNumbers.js
@@ -4,20 +4,15 @@
  * @param {number[]} nums
  * @return {number}
  */
- var findNumbers = function(nums) {
-
-
-    var totalEven= 0;
-    for(var i = 0; i < nums.length; i++){
-        if(nums[i] %2===0){
-            totalEven++;
-        
-        }
+var findNumbers = function (nums) {
+  var totalEven = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i].toString().length % 2 === 0) {
+      totalEven++;
     }
+  }
 
-    return totalEven;
-    
+  return totalEven;
 };
 
-
-console.log(findNumbers([12,345,2,6,7896]));
+console.log(findNumbers([12, 345, 2, 6, 7896]));
