b0b608900900b636be01367ae3616407a1b42561
suraj200981 <suraj200981@yahoo.co.uk>
2022-01-31 17:34:08 +0000

    max consecutive one binary completed and the code is perfect

--- Arrays 101/MaxConsecutiveOnes.js
+++ Arrays 101/MaxConsecutiveOnes.js
@@ -1,34 +1,16 @@
-var findMaxConsecutiveOnes = function (nums) {
-  var count = 1;
-
-  var newArr = [];
-
-  if (nums.length == 1) {
-    return nums[0];
-  }
-
-  for (var i = 0; i < nums.length; i++) {
-    if (nums[i + 1] != nums[i] && i != nums.length) {
-      if (count > 1) {
-        newArr.push(count);
-      }
-      count = 1;
-    }
-    if (nums[i + 1] == nums[i] && nums[i] != 0) {
-      count++;
-    }
-  }
-
-  if (newArr.length == 0) {
-    return 0;
-  }
-  var sortedArr = newArr.sort();
-
-  if (sortedArr.length > 1) {
-    return sortedArr[sortedArr.length - 1];
-  }
-
-  return sortedArr[0];
+var findMaxConsecutiveOnes = function(nums) {
+  let largestCount = 0;
+  let currentCount = 0;
+  for (let i = 0; i<nums.length; i++) {
+      if (nums[i] === 0) {
+          currentCount = 0;
+      } else {
+          currentCount++;
+      };
+      if (currentCount > largestCount) largestCount = currentCount;
+  };
+  return largestCount;
 };
 
-console.log(findMaxConsecutiveOnes([0, 0]));
+
+console.log(findMaxConsecutiveOnes([1,1]))
